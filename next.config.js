a9162660ac3c8dabb024f40650dd4438f3ccf63e
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2021-12-08 02:40:20 +0000

    Security headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,55 @@
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: "default-src 'self'",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "Server",
+    value: "Apache", // fake server value
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "sameorigin",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+];
+
 module.exports = {
   reactStrictMode: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: "/",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
