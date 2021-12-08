3cae8b1a6e0ab5b7a6a03b0ea59128311a11b2de
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2021-12-08 03:10:24 +0000

    Add vercel analytics to CSP policy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const securityHeaders = [
   {
     key: "Content-Security-Policy",
     value:
-      "default-src 'self'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src fonts.gstatic.com; img-src 'self' data:;",
+      "default-src 'self'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src fonts.gstatic.com; img-src 'self' data:; connect-src 'self' vitals.vercel-insights.com",
   },
   {
     key: "Permissions-Policy",
