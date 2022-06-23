632d482f5d690d723eaec812697fe45647f7b4a1
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-06-23 16:57:45 +0100

    Fix production build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 // pages/_document.js
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
