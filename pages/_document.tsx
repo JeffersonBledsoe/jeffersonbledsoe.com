e4dd62020a5185eb461fbbfbe1e11583e1d02f12
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-06-23 02:55:30 +0100

    Fix fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css?family=Cabin&family=Ibarra+Real+Nova&family=Playfair+Display&family=Public+Sans&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Public+Sans&display=swap"
             rel="stylesheet"
           />
           <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
